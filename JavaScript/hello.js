26edbd3f5d8b84bbe7171be8703d59ea35579963
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-08-03 17:15:42 +0530

    First Code added

---
+++ JavaScript/hello.js
@@ -0,0 +1,20 @@
+console.log("Hello world") 
+let a ,b ,c
+a = 1000
+b = 20
+c = 122
+
+if(a>b && a>c){
+    console.log("a is greter")
+    console.log(add(a,b))
+}
+else if(b>a && b>c){
+    console.log("b is greter")
+}
+else{
+    console.log("c is greter")
+}
+//write an function of aadition of two number
+function add(a,b){
+    return a+b
+}
