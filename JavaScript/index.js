49053b745ccf5a8b8ca406fd93a7900fa2bab553
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-09-21 21:20:59 +0530

    Index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,95 @@
-console.log("Hello world")
+"use strict"
+console.log("WELCOME TO THE WORLD OF JAVASCRIPT PROGRAMMING !");
 
 let a = 10 ;
+console.log("The vale of a =",a); 
+
 let b = 100;
+console.log("The Value of b =",b);
 
 let sum = a + b;
 
-console.log(sum);
+console.log("The sum of a and b =  ",sum);
 
+// INCREMENT THE VALUE OF NUMBER 
+let num = 10 ;
+
+num = 20 ;
+
+num = num + 10 ;
+
+console.log("After incremental num :",num);
+
+/* Multiline 
+comment
+in
+Javascipt 
+
+*/
+
+var stringss = "umbersss"
+const st =  typeof(stringss)
+
+console.log(st);
+
+let  type = typeof(num);
+
+console.log(type);
+
+let num1 =
+`Sagar 
+Prakash 
+Giradkar`;
+console.log(num1);
+
+
+// Syntactic error
+
+// logicak - bugs
+
+let num2 = 2 * 3;
+
+console.log("Multiplication of two number :",num2);
+
+// Always used the syntax of the declaration of variable
+
+let user = "Sagar";
+console.log(user);
+
+let num3 = 100;
+let num4 = 100;
+
+console.log(num3 % num4);
+
+let x = 6
+
+console.log(x)
+
+console.log(typeof(x));
+
+let y = 3.434
+
+console.log(typeof(y));
+
+let z 
+
+console.log(typeof(z));
+
+let value = "9786";
+
+console.log(typeof(value));
+
+let name1 = Number("Sagar Praksh Giradkar");
+// NaN (Not A Number ) its is a illegal number 
+console.log(typeof(name1));
+console.log(name1);
+
+let value3 = parseInt("484645454654475358");
+
+console.log(value3);
+
+let sagar = 10 ;
+sagar += 154544;
+sagar++;
+sagar--
+console.log(sagar)
