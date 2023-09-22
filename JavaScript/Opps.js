058923ddc309d4b44e044158197cb79730fc6e2d
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-09-22 09:07:01 +0530

    Child Class created

--- JavaScript/Opps.js
+++ JavaScript/Opps.js
@@ -6,9 +6,12 @@ console.log("Object orieneted programming in JavaScript");
 //inheritance
 //pass by value and reference
 //coding practice
-
+//Class is ued to define the object behaviour
 class Person{
+    //a class can have only one constructor
+    //you need to define all the atributes of thec lass 
     constructor(name,age,high_qual,desig,city,nationality,salary,gender){
+        //this keyword is used to point the current object of the class
         this.name = name;
         this.age = age
         //this.name -> Member of this class
@@ -41,8 +44,17 @@ class Person{
         console.log(`${this.gender}`);
     }
 }
-
+//creation of an object of a class using new keyword
 let person1 = new Person("Sagar",21,"B.Tech","Software Engineer","Pune","Indian",50000,"Male");
 person1.greet();
 person1.print();
 
+class Students extends Person{
+
+}
+
+
+let student1 = new Students("Pavan",55,"B.Tech","Android Devloper","Chinees","China",1000000,"Male");
+console.log("Printing Data From Child class by calling to the Parent class")
+student1.greet();
+student1.print();
