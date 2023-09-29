da7cbedf81fd756667cc5d9e5faed5fb6452489f
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-09-29 13:17:18 +0530

    Exception Handling And Debugging

---
+++ JavaScript/exceptionHand.js
@@ -0,0 +1,54 @@
+// exception - an unexpected  issue occuured
+
+// let a = !undefined;
+
+// if(a != undefined){
+//     throw new Error("This is Not undefined text");
+// }
+// else{
+//     console.log("This is undefined");
+// }
+
+
+
+// let a = 0;
+
+// if(a == 0){
+//     throw new Erro("Please enter a non zero value");
+// }
+// else{
+//     console.log("This is undefined");
+// }
+
+
+//Try And Catch
+
+/*
+part -1 regular code
+if there is some issue in regular code , create a code to handle issue
+*/
+/*
+try{
+    let a = 41;
+    console.log("We are inside the try block");
+    blochchain();
+}
+catch(error){
+    console.log("something went wrong ");
+}
+
+*/
+
+try{
+    let a = 5/0;
+    console.log(a);
+    console.log("We Are inside the try block")
+}
+catch(error){
+    console.log(error.name);
+    console.log(error.message);
+}
+
+finally{
+    console.log("This code is always executed")
+}
