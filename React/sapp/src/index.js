d8f816f4c0058977cc566d24627bccf4a5b7de19
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-10-02 02:03:38 +0530

    spp

--- React/sapp/src/index.js
+++ React/sapp/src/index.js
@@ -1,3 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css'
+function Ineuron()
+{
+  return (
+    <div className='sagar'>
+      <Blockchain/>
+      <ReactJS/>
+      <Blockchain/>
+      <ReactJS/>
+      <Blockchain/>
+      <ReactJS/>
+      <Blockchain/>
+      <ReactJS/>
+      <Blockchain/>
+      <ReactJS/>
+      <Blockchain/>
+      <ReactJS/>
+      <Blockchain/>
+      <ReactJS/>
+      <Blockchain/>
+      <ReactJS/>
+    </div>
+  )
 
+
+}
+const Blockchain = () =>{
+  return (
+    <div>
+      <h1>iNeuron Bengluru</h1>
+      <h4>BlockChain + React is Deadly</h4>
+    </div>
+  )};
+
+  const ReactJS = () => {
+    return (<h1>Hello Welcome to the World Of React js Components</h1>)
+  }
+
+  
+
+ReactDOM.render(<Ineuron/>,
+document.getElementById('root'));
