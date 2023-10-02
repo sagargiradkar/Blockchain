6a28134a5b98306acc8b051b34b0831906d3e2b7
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-10-02 13:36:28 +0530

    solidity code added

--- Solidity/enum.sol
+++ Solidity/enum.sol
@@ -2,14 +2,17 @@
 
 pragma solidity ^0.8.6;
 
-contract enum1 {
-    enumFreshJuice{small.medium,large};
-    FreshJuice choice;
-    FreshJuice constant default = FreshJuice.medium;
+contract enum11 {
+    enum FreshJuice{small,medium,large} FreshJuice choice;
+    FreshJuice constant defaultChoice = FreshJuice.medium;
 
 
     function setLarge() public{
         choice = FreshJuice.large;
+    }
+
+    function setSmall() public{
+        choice = FreshJuice.small;
     }
 
     function getChoice() public view returns (FreshJuice){
@@ -20,3 +23,5 @@ contract enum1 {
         return uint(defaultChoice);
     }
 }
+
+
---
+++ Solidity/structure.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.6;
+
+contract structure{
+    struct Book{
+        string title;
+        string author;
+        uint bookId;
+        uint price;
+
+    }
+
+// define a struct - name of the struct variable to represent the struct
+    Book book;
+
+    function setBook() public {
+        book = Book("Blockchain for begineers","Ineuron",4,10100);
+    }
+
+    function getBookId() public view returns(uint){
+        return book.bookId;
+    }
+
+    function getBookName() public view returns(string){
+        return book.title;
+    }
+
+
+}
