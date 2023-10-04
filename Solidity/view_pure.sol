aaee092a96da073b3e4cd34b66add4d1ecff5a13
Sagar Giradkar <spgiradkar2002@gmail.com>
2023-10-04 23:09:30 +0530

    Solidity view and pure function

---
+++ Solidity/view_pure.sol
@@ -0,0 +1,7 @@
+// //SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+// view function declares that no states will be changed 
+// pure function declare no state will be changed or read
+
