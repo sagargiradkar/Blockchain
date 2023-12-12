c9582211eadebc1967617f169422cd3d2b40b568
sagargiradkar <spgiradkar2002@gmail.com>
2023-12-12 22:25:11 +0530

    controller

--- React/awsomeapp/src/ControllerInput.js
+++ React/awsomeapp/src/ControllerInput.js
@@ -1,13 +1,27 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 
 const ControlledInput = () => {
     const [firstName, setfirstName] = useState('');
     const [email, setEmail] = useState('');
+    const[people,setPeople] = useState([]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("Hello leta submit")
+        if(firstName && email){
+            const person = {firstName,email};
+            console.log(person);
+            setPeople((people)=>{
+                return [...people,people]
+            })
+            setfirstName('');
+            setEmail('');
+
+        }
+        else{
+            console.log("Hello leta submit");
+        }
+        
     }
     return (
         <>
@@ -42,8 +56,19 @@ const ControlledInput = () => {
                         <button onClick={handleSubmit} type="submit">App Person</button>
                     </div>
                 </form>
-
-
+                {
+                    people.map((person,index)=>{
+                        const{id,firstName,email}=person
+                        return <div key={index}>
+                            <h2>
+                                {firstName}
+                            </h2>
+                            <h2>
+                                {email}
+                            </h2>
+                        </div>
+                    })
+                }
             </article>
         </>
     )
