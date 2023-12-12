babfb08425e48e11221da2c709126e578627b369
sagargiradkar <spgiradkar2002@gmail.com>
2023-12-12 23:11:39 +0530

    fallback

--- Solidity/fallbackReceive.sol
+++ Solidity/fallbackReceive.sol
@@ -3,5 +3,24 @@ pragma solidity ^0.8.18;
 
 contract FallbackReceive
 {
-    
+    string public called = " ";
+    uint public amount = 0;
+    bytes public data ;
+
+    receive() external payable 
+    { 
+        called = "receive";
+        amount = msg.value;
+    }
+
+    fallback() external payable 
+    {
+        called = "fallback";
+        amount = msg.value;
+        data = msg.data;
+    }
+}
+
+constant B{
+
 }
